6bf442d2ce2df69db929ac8f68d82606ac5885db
Shivam Shashank <shivam.shashank@juspay.in>
2024-09-10 16:45:14 +0530

    release: Android SDK v1.0.2 (#9)

--- app/src/main/java/io/hyperswitch/react/MainApplication.kt
+++ app/src/main/java/io/hyperswitch/react/MainApplication.kt
@@ -3,9 +3,11 @@ package io.hyperswitch.react
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 import com.microsoft.codepush.react.CodePush
@@ -13,52 +15,36 @@ import io.hyperswitch.BuildConfig
 
 open class MainApplication : Application(), ReactApplication {
 
-    // Initialize ReactNativeHost with DefaultReactNativeHost
-    private val mReactNativeHost: ReactNativeHost =
+    override val reactNativeHost: ReactNativeHost =
         object : DefaultReactNativeHost(this) {
+            override fun getPackages(): List<ReactPackage> =
+                PackageList(this).packages.apply {
+                    // Packages that cannot be autolinked yet can be added manually here, for example:
+                    // add(MyReactNativePackage())
+                    add(HyperPackage())
+                }
 
-            // Override getPackages to include custom packages
-            override fun getPackages(): List<ReactPackage> {
-                // Get the list of auto-linked packages
-                val packages = PackageList(this).packages
-                // Add custom packages
-                packages.add(HyperPackage())
-                return packages
-            }
-
-            // Specify the main module name
             override fun getJSMainModuleName(): String = "index"
 
-            // Enable or disable developer support based on build type
             override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-            // Enable new architecture based on build config
             override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
             override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
 
-            // Override method to get JS bundle file
-            override fun getJSBundleFile(): String? {
-                // Override the app version for CodePush
-                CodePush.overrideAppVersion(BuildConfig.VERSION_NAME)
-                // Return the JS bundle file from CodePush
+            override fun getJSBundleFile(): String {
                 return CodePush.getJSBundleFile("hyperswitch.bundle")
             }
         }
 
-    // Override method to get React Native Host
-    override fun getReactNativeHost(): ReactNativeHost {
-        return mReactNativeHost
-    }
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
-    // Override method to perform application initialization
     override fun onCreate() {
-        // Set React instance holder for CodePush
-        CodePush.setReactInstanceHolder { mReactNativeHost.reactInstanceManager }
+        CodePush.setReactInstanceHolder { reactNativeHost.reactInstanceManager }
         super.onCreate()
-        // Initialize SoLoader
         SoLoader.init(this, false)
-        // Check if new architecture is enabled and load native entry point
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // If you opted-in for the New Architecture, we load the native entry point for this app.
             load()
         }
     }
