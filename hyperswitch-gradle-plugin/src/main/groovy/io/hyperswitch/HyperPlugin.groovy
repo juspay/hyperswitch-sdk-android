d4d85a4cfd3fa753b022d1c4261daa8637b56ea2
sh-iv-am <shivam.shashank@juspay.in>
2024-12-02 20:31:02 +0530

    Gradle Plugin update

--- hyperswitch-gradle-plugin/src/main/groovy/io/hyperswitch/HyperPlugin.groovy
+++ hyperswitch-gradle-plugin/src/main/groovy/io/hyperswitch/HyperPlugin.groovy
@@ -7,7 +7,7 @@ class HyperPlugin implements Plugin<Project> {
     void apply(Project project) {
         project.plugins.withId('com.android.application') {
             project.dependencies {
-                implementation 'io.hyperswitch:hyperswitch-sdk-android:1.0.8'
+                implementation 'io.hyperswitch:hyperswitch-sdk-android:1.0.9'
             }
 
             try {
