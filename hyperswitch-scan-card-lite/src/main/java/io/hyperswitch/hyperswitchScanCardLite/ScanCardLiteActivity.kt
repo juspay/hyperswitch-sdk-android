9cf8211ee0c9b7a709560c832d579238b255aec0
ChiragKV-Juspay <chirag.kv@juspay.in>
2025-04-15 15:15:36 +0530

    Update ScanCardLiteActivity.kt

--- hyperswitch-scan-card-lite/src/main/java/io/hyperswitch/hyperswitchScanCardLite/ScanCardLiteActivity.kt
+++ hyperswitch-scan-card-lite/src/main/java/io/hyperswitch/hyperswitchScanCardLite/ScanCardLiteActivity.kt
@@ -5,45 +5,6 @@ import io.hyperswitch.android.hscardscan.cardscan.CardScanSheetResult
 import io.hyperswitch.hyperswitchScanCardLite.BaseScanCardActivity
 import io.hyperswitch.hyperswitchScanCardLite.ScanCardCallback
 
-//
-//import android.os.Bundle
-//import androidx.appcompat.app.AppCompatActivity
-//import io.hyperswitch.android.hscardscan.cardscan.CardScanSheet
-//import io.hyperswitch.android.hscardscan.cardscan.CardScanSheetResult
-//
-//class ScanCardLiteActivity : AppCompatActivity() {
-//
-//    companion object {
-//        var callback: ScanCardCallback? = null
-//    }
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        val cardScanSheet = CardScanSheet.create(this, ::onScanFinished)
-//        cardScanSheet.present()
-//    }
-//
-//    private fun onScanFinished(result: CardScanSheetResult) {
-//        val resultMap = mutableMapOf<String, Any?>()
-//        when (result) {
-//            is CardScanSheetResult.Completed -> {
-//                resultMap["status"] = "Succeeded"
-//                val dataMap = mutableMapOf<String, String?>()
-//                dataMap["pan"] = result.scannedCard.pan
-//                resultMap["data"] = dataMap
-//            }
-//            is CardScanSheetResult.Canceled -> {
-//                resultMap["status"] = "Cancelled"
-//            }
-//            is CardScanSheetResult.Failed -> {
-//                resultMap["status"] = "Failed"
-//            }
-//        }
-//        callback?.onScanResult(resultMap)
-//        finish()
-//    }
-//}
-
 class ScanCardLiteActivity : BaseScanCardActivity() {
 
     companion object {
