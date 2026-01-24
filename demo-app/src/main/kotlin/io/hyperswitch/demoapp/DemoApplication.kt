d5b94bd9f515d2b40f4e429ed07b722dc2b117dc
Pradeep Kumar <pradeep.kumar@Pradeep-Kumar-M6LJQ60G2H.local>
2026-01-25 00:36:23 +0530

    chore: detox testing purpose

---
+++ demo-app/src/main/kotlin/io/hyperswitch/demoapp/DemoApplication.kt
@@ -0,0 +1,10 @@
+package io.hyperswitch.demoapp
+
+import io.hyperswitch.react.MainApplication
+
+/**
+ * Demo-app specific Application class for Detox E2E testing.
+ * Extends MainApplication to provide ReactApplication implementation
+ * required by Detox for synchronization.
+ */
+class DemoApplication : MainApplication()
