12bbd3d4ced22c9017233a1b5af0ece8e36e68c1
Pradeep Kumar <pradeep.kumar@Pradeep-Kumar-M6LJQ60G2H.local>
2026-01-25 01:13:45 +0530

    chore: testing for detox

--- demo-app/src/main/kotlin/io/hyperswitch/demoapp/DemoApplication.kt
+++ demo-app/src/main/kotlin/io/hyperswitch/demoapp/DemoApplication.kt
@@ -1,10 +1,47 @@
 package io.hyperswitch.demoapp
 
-import io.hyperswitch.react.MainApplication
+import android.app.Application
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.soloader.SoLoader
+import io.hyperswitch.react.HyperPackage
 
 /**
  * Demo-app specific Application class for Detox E2E testing.
- * Extends MainApplication to provide ReactApplication implementation
+ * Implements ReactApplication directly to provide React Native support
  * required by Detox for synchronization.
  */
-class DemoApplication : MainApplication()
+class DemoApplication : Application(), ReactApplication {
+
+    override val reactNativeHost: ReactNativeHost =
+        object : DefaultReactNativeHost(this) {
+            override fun getPackages(): List<ReactPackage> =
+                PackageList(this).packages.apply {
+                    add(HyperPackage())
+                }
+
+            override fun getJSMainModuleName(): String = "index"
+
+            override fun getUseDeveloperSupport(): Boolean = false
+
+            override val isNewArchEnabled: Boolean = true
+            override val isHermesEnabled: Boolean = true
+        }
+
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(applicationContext, reactNativeHost)
+
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+        if (reactNativeHost.useDeveloperSupport) {
+            load()
+        }
+    }
+}
