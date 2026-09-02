aeb429f03d3ec839dca3ef09682582be688262f9
ChiragKV-Juspay <chirag.kv@juspay.in>
2026-09-02 22:59:32 +0530

    fix: added min height to cvc widget

--- app/src/main/kotlin/io/hyperswitch/view/HyperswitchElement.kt
+++ app/src/main/kotlin/io/hyperswitch/view/HyperswitchElement.kt
@@ -31,6 +31,26 @@ open class HyperswitchElement @JvmOverloads constructor(
 
     var type: String? = null
 
+    private var heightFloorPx: Int = 0
+
+    protected fun setHeightFloor(heightDp: Float) {
+        heightFloorPx = (heightDp * resources.displayMetrics.density).toInt()
+        minimumHeight = heightFloorPx
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        if (
+            heightFloorPx > 0 &&
+            MeasureSpec.getMode(heightMeasureSpec) != MeasureSpec.EXACTLY
+        ) {
+            internalView.measure(
+                MeasureSpec.makeMeasureSpec(measuredWidth, MeasureSpec.EXACTLY),
+                MeasureSpec.makeMeasureSpec(measuredHeight, MeasureSpec.EXACTLY),
+            )
+        }
+    }
+
     init {
         addView(internalView, LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT))
     }
